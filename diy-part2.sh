759de37e9c12bbc4c85fcc5fa02344af10003d3e
yaoyue12548 <127120154+yaoyue12548@users.noreply.github.com>
2024-11-24 15:19:40 +0800

    diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,3 +19,5 @@
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+rm -rf feeds/luci/applications/luci-app-alist
+rm -rf feeds/luci/packages/net/alist
