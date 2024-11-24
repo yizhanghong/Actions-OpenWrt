a292c4ebfebfc3b8a080dab6171f257553dc0b73
yaoyue12548 <127120154+yaoyue12548@users.noreply.github.com>
2024-11-24 15:11:24 +0800

    diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,5 @@
 
 # Add a feed source
  sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
- rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 git clone https://github.com/Siriling/5G-Modem-Support package/luci-app-modem
 
